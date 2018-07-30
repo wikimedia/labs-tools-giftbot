d69e1810786d0843ad68dbcfb2ad1b6f50e6592c
Giftpflanze <gifti@tools.wmflabs.org>
2018-07-30 13:01:54 +0000

    unreviewed.sh: Add rheinneckar
    Change-Id: I4b68a053b7ea1e31acf9786de2f515afff4d1437

--- unreviewed.sh
+++ unreviewed.sh
@@ -1,6 +1,6 @@
 cd PHPWikiBot
 
-for config in antik aus egypt bawue berg bghsw bio bos chem computerspiel country dortmund ehock esc foot frei grue hockey info kanu laus linux marx math mhess motor muensterland musik myth norwegen owl phil phys pol religion sachs sauer schuldrecht schweiz segeln soft sozi spam stat street tennis tv umwelt wirt wsport
+for config in antik aus egypt bawue berg bghsw bio bos chem computerspiel country dortmund ehock esc foot frei grue hockey info kanu laus linux marx math mhess motor muensterland musik myth norwegen owl phil phys pol religion rheinneckar sachs sauer schuldrecht schweiz segeln soft sozi spam stat street tennis tv umwelt wirt wsport
 do
 	echo unreviewed-$config:
 	./PHPWikiBot.php login unreviewed config=unreviewed-$config
